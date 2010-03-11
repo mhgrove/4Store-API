6205658893609614e75803b77982cf415bbce1d1
Michael Grove <mhgrove@Michael-Groves-iMac.local>
2010-03-11 13:05:27 -0500

    adding copyright assertions to new source files

--- src/com/clarkparsia/fourstore/sesame/trans/TransactionSupport.java
+++ src/com/clarkparsia/fourstore/sesame/trans/TransactionSupport.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2009-2010 Clark & Parsia, LLC. <http://www.clarkparsia.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.clarkparsia.fourstore.sesame.trans;
 
 import org.openrdf.sail.SailConnectionListener;
@@ -13,16 +28,19 @@ public interface TransactionSupport extends SailConnectionListener {
 
 	/**
 	 * Commit the current transaction
+	 * @throws org.openrdf.sail.SailException if there is an error while committing
 	 */
 	public void commit() throws SailException;
 
 	/**
 	 * Rollback the changes in the current transaction
+	 * @throws org.openrdf.sail.SailException if there is an error rolling back operations
 	 */
 	public void rollback() throws SailException;
 
 	/**
 	 * Start a transaction
+	 * @throws org.openrdf.sail.SailException if there is an error starting the transaction
 	 */
 	public void begin() throws SailException;
 
