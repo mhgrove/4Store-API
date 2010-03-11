f0983b2169e0e34522f4c75f816dbacbcfb72739
Michael Grove <mhgrove@Michael-Groves-iMac.local>
2010-03-11 11:32:56 -0500

    new and improved shell script

--- 4store.sh
+++ 4store.sh
@@ -1,1 +1,13 @@
-java -Xmx512m -jar lib/cp-common-fourstore-0.3.1.jar
+#!/bin/sh
+
+if [ -n "${JAVA_HOME}" -a -x "${JAVA_HOME}/bin/java" ]; then
+  java="${JAVA_HOME}/bin/java"
+else
+  java=java
+fi
+
+if [ -z "${fourstore_java_args}" ]; then
+  fourstore_java_args="-Xmx512m"
+fi
+
+exec ${java} ${fourstore_java_args} -jar lib/cp-common-fourstore-0.3.1.jar "$@"
